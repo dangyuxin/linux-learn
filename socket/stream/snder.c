b1150bd7f7fcdcef9270d43eee926c49a0ef5dd3
“mejomejo” <1975815008@qq.com>
2023-05-10 18:59:04 +0800

    ok

--- socket/stream/snder.c
+++ socket/stream/snder.c
@@ -14,12 +14,38 @@ int main()
 {
     struct sockaddr_in laddr;
     int sd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sd < 0)
+    {
+        perror("socket()");
+        exit(1);
+    }
 
     laddr.sin_family = AF_INET;
     laddr.sin_port = htons(PORT);
     inet_pton(AF_INET, "0.0.0.0", &laddr.sin_addr);
 
-    bind(sd, &laddr, sizeof(laddr));
+    // bind();
 
-    listen(sd, 300);
+    if (connect(sd, (void *)&laddr, sizeof(laddr)) < 0)
+    {
+        perror("connect");
+        exit(1);
+    }
+
+    FILE *fp = fdopen(sd, "r");
+    if (fp == NULL)
+    {
+        perror("fdopen()");
+        exit(1);
+    }
+
+    long long stamp;
+
+    if (fscanf(fp, STAMP, &stamp) < 1)
+        fprintf(stderr, "Bad format!\n");
+    else
+        fprintf(stdout, "%lld\n", stamp);
+
+    fclose(fp);
+    close(sd);
 }
